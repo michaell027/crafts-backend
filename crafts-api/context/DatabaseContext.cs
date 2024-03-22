b11c81aab21463c3eb4d737984ee0bc94a2cd073
Michaela Majorošová <miselka12345@gmail.com>
2024-03-22 09:27:34 +0100

    override function on model creating in dbcontext

--- crafts-api/context/DatabaseContext.cs
+++ crafts-api/context/DatabaseContext.cs
@@ -3,17 +3,43 @@ using Microsoft.EntityFrameworkCore;
 
 namespace crafts_api.context;
 
-public class DatabaseContext:DbContext
+public class DatabaseContext : DbContext
 {
     private IConfiguration _configuration;
-    
+
     public DatabaseContext(IConfiguration configuration) => _configuration = configuration;
-    
+
     public DbSet<Category> Categories { get; set; }
+    public DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         var connectionString = _configuration.GetConnectionString("Default");
         optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.PublicId).IsRequired();
+            entity.Property(e => e.Name).IsRequired();
+            entity.Property(e => e.SkName).IsRequired();
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.PublicId).IsRequired();
+            entity.Property(e => e.FirstName).IsRequired();
+            entity.Property(e => e.LastName).IsRequired();
+            entity.Property(e => e.Password).IsRequired();
+            entity.Property(e => e.Email).IsRequired();
+            entity.Property(e => e.CreatedAt).IsRequired();
+            entity.Property(e => e.UpdatedAt).IsRequired();
+        });
     }
 }
