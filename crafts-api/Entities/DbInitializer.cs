9dba97ef317a603c9b76fe5a8fbdd380925e9132
Michaela Majorošová <miselka12345@gmail.com>
2024-03-22 09:30:02 +0100

    Create DbInitializer.cs

---
+++ crafts-api/Entities/DbInitializer.cs
@@ -0,0 +1,34 @@
+﻿using crafts_api.context;
+using crafts_api.models.domain;
+
+namespace crafts_api.models
+{
+    public class DbInitializer
+    {
+        public static void Seed(DatabaseContext context)
+        {
+
+            if (!context.Categories.Any())
+            {
+                context.AddRange(
+                    new Category
+                    {
+                        Name = "Woodworking",
+                        SkName = "Drevo"
+                    },
+                    new Category
+                    {
+                        Name = "Metalworking",
+                        SkName = "Kováčstvo"
+                    },
+                    new Category
+                    {
+                        Name = "Leatherworking",
+                        SkName = "Kožušníctvo"
+                    }
+                                                                                            );
+                context.SaveChanges();
+            }
+        }
+    }
+}
