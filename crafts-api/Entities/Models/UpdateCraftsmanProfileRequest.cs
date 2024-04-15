c3a267580274c8be04cf355e07fada1a56726bf5
Michaela Majorošová <miselka12345@gmail.com>
2024-04-15 13:53:06 +0200

    create UpdateCraftsmanProfileRequest

--- crafts-api/Entities/Models/UpdateCraftsmanProfileRequest.cs
+++ crafts-api/Entities/Models/UpdateCraftsmanProfileRequest.cs
@@ -2,5 +2,29 @@
 
 public class UpdateCraftsmanProfileRequest
 {
+    public string Username { get; set; } = string.Empty;
     
+    public string FirstName { get; set; } = string.Empty;
+    
+    public string LastName { get; set; } = string.Empty;
+    
+    public string Email { get; set; } = string.Empty;
+    
+    public string Bio { get; set; } = string.Empty;
+    
+    public string PhoneNumber { get; set; } = string.Empty;
+    
+    public string Address { get; set; } = string.Empty;
+    
+    public string City { get; set; } = string.Empty;
+    
+    public string Country { get; set; } = string.Empty;
+    
+    public string Street { get; set; } = string.Empty;
+    
+    public string Number { get; set; } = string.Empty;
+    
+    public string PostalCode { get; set; } = string.Empty;
+    
+    public string ProfilePicture { get; set; } = string.Empty;
 }
