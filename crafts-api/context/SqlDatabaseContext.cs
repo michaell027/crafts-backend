31cbf1caf70970e78624a0c1eefd148f32faf82a
Michaela Majorošová <miselka12345@gmail.com>
2024-04-17 13:37:03 +0200

    create db context for development environment

---
+++ crafts-api/context/SqlDatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crafts_api.context;
+
+// dotnet ef migrations add MigrationName --context SqlDatabaseContext --output-dir Migrations/SqlServerMigrations
+// dotnet ef database update --context SqlDatabaseContext
+public class SqlDatabaseContext : DatabaseContext
+{
+    private readonly IConfiguration configuration;
+
+        public SqlDatabaseContext(IConfiguration configuration) : base(configuration)
+        {
+            this.configuration = configuration;
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = configuration.GetConnectionString("Default");
+            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+}
